9c2c29205e599b92aa30a5adb357f806056cfce6
JuanCarlosBajan <baj20109@uvg.edu.gt>
2021-03-10 16:01:57 -0600

    Prueba de commit

--- src/ControlTest.java
+++ src/ControlTest.java
@@ -2,7 +2,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ControlTest {
 
+    Control control = new Control();
+
     @org.junit.jupiter.api.Test
     void process() {
+
+        control.Process("(+ (-5 4))");
     }
 }
