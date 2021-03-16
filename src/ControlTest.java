45a91a80953be0ea4a76d262c4497dd3012a07f3
JuanCarlosBajan <baj20109@uvg.edu.gt>
2021-03-15 19:53:08 -0600

    commit por si a caso falta subir algo

--- src/ControlTest.java
+++ src/ControlTest.java
@@ -7,7 +7,7 @@ class ControlTest {
     @org.junit.jupiter.api.Test
     void process() {
 
-        control.Process("(+ (+ (+ (+ 5 (* 5 (- 6 7))) 5) 5) (+ 5 5))");
+        control.Process("(+ (+ 10 5 9) 8)");
 
     }
 }
