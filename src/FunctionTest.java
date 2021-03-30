8a25ee80d7c75642634be97527a348c9c03555d7
JuanCarlosBajan <baj20109@uvg.edu.gt>
2021-03-30 15:24:16 -0600

    Finalizacion del funcionamiento y estructura 2.0

--- src/FunctionTest.java
+++ src/FunctionTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class FunctionTest {
 
     Control control = new Control();
-    Function f = new Function("(defun factorial (x) (cond ((equal x 1) 1) ((equal x 0) 1) (t (* (factorial (- x 1)) x ))))");
-    Function h = new Function("(defun fibo (x) (cond ((equal x 0) 0) ((equal x 1) 1) (t (+ (fibo (- x 1)) (fibo (- x 2))) )))");
+    //Function f = new Function("(defun factorial (x) (cond ((equal x 1) 1) ((equal x 0) 1) (t (* (factorial (- x 1)) x ))))");
+    //Function h = new Function("(defun fibonacci (x) (cond ((equal x 0) 0) ((equal x 1) 1) (t (+ (fibonacci (- x 1)) (fibonacci (- x 2))) )))");
 
     OpAritmeticas aritmetics = new OpAritmeticas();
 
@@ -16,8 +16,10 @@ class FunctionTest {
         Control.using.put("c", "6");
         //System.out.println(control.Process("(halo 5)"));
         //System.out.println(f.Process("asd", "(halo 5) 6 (+ (cond ((equal x 4) 22) ((equal x 0) 1) (t (* (factorial (- x 1)) x ))) (+ (* c 3) x (+ 5 5 5)) 7) 12"));
-
-        System.out.println(control.Process("(fibo 21)"));
+        control.Process("(defun fibonacci (x) (cond ((equal x 0) 0) ((equal x 1) 1) (t (+ (fibonacci (- x 1)) (fibonacci (- x 2))) )))");
+        control.Process("(defun factorial (x) (cond ((equal x 1) 1) ((equal x 0) 1) (t (* (factorial (- x 1)) x ))))");
+        System.out.println(control.Process("(fibonacci 10)"));
+        System.out.println(control.Process("(factorial 10)"));
 
     }
 
---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Control control = new Control();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bienvenido a un interprete de LISP\n Por favor ingrese el codigo que desea procesar\n(ingrese 'SALIR', para salir):");
+
+        boolean run = true;
+        while (run){
+            String ans = sc.nextLine();
+            if(ans.toLowerCase().equals("salir")){
+                System.out.println("Saliendo...");
+                run = false;
+            }
+            if (Analyze(ans) && run){
+                System.out.println(control.Process(ans));
+            }
+
+        }
+    }
+
+    private static boolean Analyze(String expression){
+        Boolean ans = false;
+
+        int opening = 0;
+
+        for(int x = 0; x < expression.length(); x++){
+            if(expression.charAt(x) == '('){
+                opening++;
+            } else if (expression.charAt(x) == ')'){
+                opening--;
+            }
+        }
+
+        if (opening == 0){
+            ans = true;
+        }
+
+        String[] verif = expression.split(" ");
+
+        for (String s: verif){
+            if(s.equals("")){
+                ans = false;
+            }
+        }
+
+        if (!ans){
+            System.out.println("\nSe ha encontrado un problema de escritura en su codigo.");
+        }
+
+        return ans;
+    }
+}
